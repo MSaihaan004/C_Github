dafa96f58ef82d7270e9ecd7c69f67e4cf72ba27
MSaihaan004 <m.saihaan004@gmail.com>
2025-05-29 12:09:40 +0530

    Add files via upload

---
+++ ContOfIntArray.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int i;
+    int arr[] = {10,20,30,40,50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Array elements are : \n");
+
+    for (i=0;i<n;i++) {
+        printf("%d ", arr[i]);
+
+    }
+    return 0;
+}
---
+++ DisplayContOfCharArr.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+
+    char arr[] = "Hello";
+    printf("Character array content: %s\n", arr);
+
+    return 0;
+}
